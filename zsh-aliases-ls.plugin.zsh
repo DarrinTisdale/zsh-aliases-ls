fdebd765ca05231e1dba4ed03d7f95105433dc42
Darrin Tisdale <darrin.tisdale@gmail.com>
2019-04-12 19:13:58 -0400

    Added initial plugin script file

---
+++ zsh-aliases-ls.plugin.zsh
@@ -0,0 +1,19 @@
+#
+# Project:  zsh-aliases-ls
+# File:     /zsh-aliases-ls.plugin.zsh
+# Created:  2019-04-12 19:07:28
+# Author:   Darrin Tisdale
+# -----
+# Modified: 2019-04-12 19:09:40
+# Editor:   Darrin Tisdale
+#
+
+alias l='ls -lFh'          #size,show type,human readable
+alias la='ls -lAFh'        #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'        #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'        #long list,sorted by date,show type,human readable
+alias ll='ls -l'           #long list
+alias ldot='ls -ld .*'
+alias lS='ls -1FSsh'
+alias lart='ls -1Fcart'
+alias lrt='ls -1Fcrt'
